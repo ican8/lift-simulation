4b67834471db956982491dff1caa285e17a19964
soureesh <soureeshpatil@gmail.com>
2021-12-15 12:10:36 +0530

    added icons, removed logs

--- js/index.js
+++ js/index.js
@@ -1,21 +1,14 @@
-// create lift
+let lifts = []
+let currentFloor = 1;
+let input_floors = document.getElementById("input-floors");
+let input_lifts = document.getElementById("input-lifts");
+let no_of_floors;
+let no_of_lifts;
+
 let form = document.getElementById('form')
 form.addEventListener('click',function(e){
   e.preventDefault();
 })
-
-let lifts = []
-
-// let lift = document.createElement("div");
-// lift.className = "lift";
-// lift.id = "l1";
-// let left_door = document.createElement("div");
-// let right_door = document.createElement("div");
-// left_door.classList.add("door","door-left")
-// right_door.classList.add("door","door-right")
-// lift.appendChild(left_door)
-// lift.appendChild(right_door)
-
 
 function createLifts(n){
   lifts = []
@@ -35,8 +28,6 @@ function createLifts(n){
     lifts.push(lift)
   }  
 }
-
-let currentFloor = 1;
 
 function createFloor(floor_number) {
   let container = document.getElementById("container");
@@ -76,9 +67,7 @@ function createFloor(floor_number) {
 
 function closeDoor(e){
   let target_id = e.target.id;
-  // NOTE :- BAD ASSUMPTION , MAX LIFTS = 9
   let lift_no = target_id[target_id.length-1]
-  console.log("LIFT TARGET = ",lift_no);
   let left_door = document.getElementById('ld'+lift_no);
   let right_door = document.getElementById('rd'+lift_no);
   left_door.style.transform = `translateX(0)`;
@@ -91,18 +80,15 @@ function doorAnimation(e){
   let target_id = e.target.id;
   // NOTE :- BAD ASSUMPTION , MAX LIFTS = 9
   let lift_no = target_id[target_id.length-1]
-  console.log("LIFT TARGET = ",lift_no);
   let lift = document.getElementById('l'+lift_no)
   lift.removeEventListener('webkitTransitionEnd',doorAnimation)
-  // console.log('START DOOR ANIMATION!')
   let left_door = document.getElementById('ld'+lift_no);
   let right_door = document.getElementById('rd'+lift_no);
   right_door.addEventListener('webkitTransitionEnd',closeDoor,lift_no)
   left_door.style.transform = `translateX(-100%)`;
   right_door.style.transform = `translateX(100%)`;
   left_door.style.transition = `all 2.5s ease-out`;
-  right_door.style.transition = `all 2.5s ease-out`;
-  
+  right_door.style.transition = `all 2.5s ease-out`;  
 }
 
 function scheduledLift(){
@@ -116,28 +102,16 @@ function moveLift(e) {
     n = Number(clicked_on.substring(2,clicked_on.length));
   else if(clicked_on.startsWith('down'))
     n = Number(clicked_on.substring(4,clicked_on.length));
-  console.log('go to floor ',n);
   let distance = -1*(n-1)*100 ;
 
   let lift_no = scheduledLift();
-  console.log('RANDOMLY SELECTED LIFT = ',lift_no);
   let lift = document.getElementById('l'+lift_no)
-  console.log('HERE LIFT = ',lift)
-  // let right_door = document.getElementsByClassName('door-right')[0];
   lift.addEventListener('webkitTransitionEnd',doorAnimation,lift_no)
   lift.style.transform = `translateY(${distance}%)`
   
-  // lift.style.transform = `translateY(${-620 }%)`
   let time = 2 * Math.abs(currentFloor - n);
   lift.style.transitionDuration = `${time}s`;
-  console.log('ETA- ',Math.abs(currentFloor - n));
   currentFloor = n;
-  // console.log('Current floor - ',c);
-  // let target_floor = document.getElementById("floor" + n);
-  // target_floor.insertBefore(
-  //   lift,
-  //   target_floor.childNodes[target_floor.childNodes.length - 1]
-  // );
 }
 
 function getButtons(){
@@ -154,11 +128,6 @@ function getButtons(){
     }
 }
 
-let input_floors = document.getElementById("input-floors");
-let input_lifts = document.getElementById("input-lifts");
-let no_of_floors;
-let no_of_lifts;
-
 function make_floors() {
   container.innerHTML = "";
   no_of_floors = input_floors.value;
@@ -167,17 +136,13 @@ function make_floors() {
     createFloor(i);
   }
   createLifts(no_of_lifts);
-  console.log(lifts);
 
   for(lift of lifts){
     lift.style.transform = null
     lift.style.transitionDuration = null
-    // set initial position of lift
-    console.log(lift);
   }
 
   let first_floor = document.getElementById("floor1");
-  console.log(first_floor);
 
   for(let i = lifts.length-1; i >= 0; i--){
     first_floor.insertBefore(
